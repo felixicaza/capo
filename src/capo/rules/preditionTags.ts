370ab177742e2eafb9fdc27931f4cf3c33553c33
Felix Icaza <fx.joliett17@gmail.com>
2026-05-22 15:25:11 -0600

    fix: update classification weight for speculationrules

--- src/capo/rules/preditionTags.ts
+++ src/capo/rules/preditionTags.ts
@@ -3,13 +3,13 @@ import type { Attributes } from '../types/index.ts'
 import { hasRel, hasType } from '../helpers/index.ts'
 
 const PredictionWeight = {
-  PREFETCH: 1,
-  SPECULATIONRULES: 0
+  SPECULATIONRULES: 1,
+  PREFETCH: 0
 } as const
 
 export function classifyPredictionTags(name: string, attributes: Attributes): number | undefined {
-  if (name === 'link' && hasRel(attributes, 'prefetch')) return PredictionWeight.PREFETCH
   if (name === 'script' && hasType(attributes, 'speculationrules')) return PredictionWeight.SPECULATIONRULES
+  if (name === 'link' && hasRel(attributes, 'prefetch')) return PredictionWeight.PREFETCH
 
   return undefined
 }
